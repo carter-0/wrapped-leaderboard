430921becf90824e54996d46b9eea38005d31b08
carter-0 <carterannandale@gmail.com>
2024-12-04 13:25:15 +0000

    feat: add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,20 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DM3FMXYMGN"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-DM3FMXYMGN');
+            `}
+        </Script>
       </body>
     </Html>
   );
